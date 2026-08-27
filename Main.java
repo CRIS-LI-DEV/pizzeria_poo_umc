01ca2366cd0eb0e416bf96972dc34cb4fdc88b72
Victor Torres <hugotorressoto15@gmail.com>
2026-08-27 15:59:54 -0400

    Agrega comentarios explicativos a la clase Main

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-      
+        // Se instancia un objeto de la clase Pizza pasando los atributos al constructor
         Pizza miPizza = new Pizza("Pepperoni Especial", "Familiar", 12990.0);
 
-      
+        // Se invoca el método de la clase para imprimir los datos en la consola
         miPizza.mostrarInfo();
     }
 }
