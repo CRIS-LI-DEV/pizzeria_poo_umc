27290b0b5a9168cf9d9f599acdf1c2143f10554a
CRISTIAN LILLO CESPEDES <cristianlillocespedes@gmail.com>
2026-09-01 21:58:35 -0400

    INSTACIAS PERSONA

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Se instancia un objeto de la clase Pizza pasando los atributos al constructor
+        
         Pizza miPizza = new Pizza("Pepperoni Especial", "Familiar", 12990.0);
 
-        // Se invoca el método de la clase para imprimir los datos en la consola
+      
         miPizza.mostrarInfo();
 
         System.out.println("hice un cambio");
-        
+
         Persona p1 = new Persona("12.345.678-9", "  Carlos Muñoz  ", "987654321", "carlos@example.com");
         p1.mostrarContacto();
         System.out.println("Teléfono válido: " + p1.tieneTelefonoValido());
